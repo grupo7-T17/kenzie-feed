8f9ff2cf6620ba66a716b852a30d95330e81defe
JhonnatanDouglas <jhonnatanaraujoDEV@gmail.com>
2023-07-02 16:36:14 -0300

    style: cria styles default

---
+++ src/styles/grid.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const StyledContainerHeader = styled.header`
+  margin: auto;
+  width: 100%;
+  max-width: 76.875rem;
+  padding: 0.875rem 0.75rem 0rem;
+`;
+
+export const StyledContainerMain = styled.main`
+  margin: 0 auto;
+  width: 100%;
+  max-width: 76.875rem;
+  padding: 0rem 1.125rem 1rem;
+`;
+
+export const StyledContainerEdit = styled.main`
+  margin: 0 auto;
+  width: 100%;
+  max-width: 57.5rem;
+  padding: 0rem 1.125rem 1rem;
+`;
+
+export const StyledContainerLogin = styled.main`
+  margin: 0 auto;
+  width: 100%;
+  max-width: 71.25rem;
+  padding: 0rem 1.125rem 1rem;
+`;
+
+export const StyledContainerRegister = styled.main`
+  margin: 0 auto;
+  width: 100%;
+  max-width: 48rem;
+  padding: 0rem 1.125rem 1rem;
+`;
