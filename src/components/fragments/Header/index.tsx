7f6d6b274e25019da4d5aee0cf5fec760fb26755
gumrrk <gustavomanrrik037@hotmail.com>
2023-07-04 06:39:41 -0300

    fix: correção de erros

--- src/components/fragments/Header/index.tsx
+++ src/components/fragments/Header/index.tsx
@@ -1,12 +1,12 @@
 import { StyledHeader, StyledIcon } from './style';
 import { StyledButton } from '../../../styles/buttons';
-import Logo from '../assets/logo.svg';
+import Logo from '../../../assets/logo.svg';
 
 export const Header = () => {
   return (
     <StyledHeader>
       <StyledIcon src={Logo} alt='Imagem' />
-      <StyledButton buttonType='primary' buttonSize='md'>Acessar</StyledButton>
+      <StyledButton buttonType='primary' buttonSize='md-max'>Acessar</StyledButton>
     </StyledHeader>
   );
 };
--- src/routes/PublicRoutes/index.tsx
+++ src/routes/PublicRoutes/index.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react"
 import { Navigate, Outlet } from "react-router-dom" 
-import { UserContext } from "../../providers"
+import { UserContext } from "../../providers/UserContext"
 
 export const PublicRoutes = () => {
     const { user } = useContext(UserContext) 
