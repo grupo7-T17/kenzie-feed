58c958c28c51421af828fd564bc2b92bb9dd71a9
gumrrk <gustavomanrrik037@hotmail.com>
2023-07-04 21:08:39 -0300

    style: cria header da dashboard

--- src/components/fragments/Header/index.tsx
+++ src/components/fragments/Header/index.tsx
@@ -1,12 +1,33 @@
-import { StyledHeader, StyledIcon } from './style';
+import {
+  StyledHeader,
+  StyledLogo,
+  StyledDivIconButton,
+  QuitButton,
+} from './style';
 import { StyledButton } from '../../../styles/buttons';
+import { IconUser } from '../IconUser';
 import Logo from '../../../assets/logo.svg';
+import Quit from '../../../assets/icons/quit.svg';
+
+const user = false;
 
 export const Header = () => {
   return (
     <StyledHeader>
-      <StyledIcon src={Logo} alt='Imagem' />
-      <StyledButton buttonType='primary' buttonSize='sm-min'>Acessar</StyledButton>
+      <StyledLogo src={Logo} alt='Logo da Kenzie Feed' />
+      {user ? (
+        <StyledDivIconButton>
+          <IconUser letter='A' />
+          <StyledButton buttonType='outline' buttonSize='sm-min'>
+            Dashboard
+          </StyledButton>
+          <QuitButton src={Quit} />
+        </StyledDivIconButton>
+      ) : (
+        <StyledButton buttonType='primary' buttonSize='sm-min'>
+          Acessar
+        </StyledButton>
+      )}
     </StyledHeader>
   );
 };
--- src/components/fragments/Header/style.ts
+++ src/components/fragments/Header/style.ts
@@ -1,15 +1,23 @@
-
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const StyledHeader = styled.header`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    height: 2.375rem;
-    max-width: 76.75rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 2.375rem;
+  max-width: 76.75rem;
 `;
 
-export const StyledIcon = styled.img`
-    width: 4.1875rem;
+export const StyledLogo = styled.img`
+  width: 4.1875rem;
 `;
 
+export const StyledDivIconButton = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+`;
+
+export const QuitButton = styled.img`
+  width: 1.5rem;
+`;
