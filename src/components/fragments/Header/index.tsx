1bbb7c7adee063d1d50471b6f693e2e0131e338b
gumrrk <gustavomanrrik037@hotmail.com>
2023-07-05 09:33:59 -0300

    fix: revisa header e loginpage

--- src/components/fragments/Header/index.tsx
+++ src/components/fragments/Header/index.tsx
@@ -11,23 +11,30 @@ import Quit from '../../../assets/icons/quit.svg';
 
 const user = false;
 
-export const Header = () => {
+interface iHeaderProps {
+  button?: boolean;
+}
+
+export const Header = ({ button }: iHeaderProps) => {
   return (
     <StyledHeader>
       <StyledLogo src={Logo} alt='Logo da Kenzie Feed' />
-      {user ? (
-        <StyledDivIconButton>
-          <IconUser letter='A' />
-          <StyledButton buttonType='outline' buttonSize='sm-min'>
-            Dashboard
+
+      {button ? (
+        user ? (
+          <StyledDivIconButton>
+            <IconUser letter='A' />
+            <StyledButton buttonType='outline' buttonSize='sm-min'>
+              Dashboard
+            </StyledButton>
+            <QuitButton src={Quit} />
+          </StyledDivIconButton>
+        ) : (
+          <StyledButton buttonType='primary' buttonSize='sm-min'>
+            Acessar
           </StyledButton>
-          <QuitButton src={Quit} />
-        </StyledDivIconButton>
-      ) : (
-        <StyledButton buttonType='primary' buttonSize='sm-min'>
-          Acessar
-        </StyledButton>
-      )}
+        )
+      ) : null}
     </StyledHeader>
   );
 };
--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,38 +1,53 @@
-import { Footer } from "../../components/fragments/Footer";
-import { Header } from "../../components/fragments/Header";
-import { StyledContainerHeader, StyledContainerMain } from "../../styles/grid";
-import pizza from "../../assets/imgs/teste-pizza.png"
-import { StyledParagraph, StyledTitleOne } from "../../styles/typography";
-import { StyledDivInputLogin, StyledDivLoginPage, StyledLinkLoginPage } from "./style";
-import { StyledButton } from "../../styles/buttons";
-import { StyledInput } from "../../styles/inputs";
-import { Link } from "react-router-dom";
+import { Footer } from '../../components/fragments/Footer';
+import { Header } from '../../components/fragments/Header';
+import { StyledContainerHeader, StyledContainerMain } from '../../styles/grid';
+import welcome from '../../assets/imgs/welcome.svg';
+import { StyledParagraph, StyledTitleOne } from '../../styles/typography';
+import {
+  StyledContainerLoginImage,
+  StyledDivInputLogin,
+  StyledDivLoginPage,
+  StyledLinkLoginPage,
+  StyledLoginImage,
+} from './style';
+import { StyledButton } from '../../styles/buttons';
+import { StyledInput } from '../../styles/inputs';
 
 export const LoginPage = () => {
-    return(
-      <StyledContainerMain>
-        <StyledContainerHeader>
-            <Header/>
-        </StyledContainerHeader>
-        <StyledDivLoginPage>
-            <img src={pizza} alt="" />
-            <StyledDivInputLogin>
-            <StyledTitleOne fontStyle='md'>
-            Acesse o KenzieFeed
-            </StyledTitleOne>
-            <StyledParagraph fontStyle='sm'>
+  return (
+    <StyledContainerMain>
+      <StyledContainerHeader>
+        <Header button={false}/>
+      </StyledContainerHeader>
+      <StyledDivLoginPage>
+        <StyledContainerLoginImage>
+          <StyledLoginImage src={welcome} alt='Imagem de Pizza' />
+        </StyledContainerLoginImage>
+        <StyledDivInputLogin>
+          <StyledTitleOne fontStyle='md'>Acesse o KenzieFeed</StyledTitleOne>
+          <StyledParagraph fontStyle='sm'>
             Preencha os campos corretamente para fazer login
-            </StyledParagraph>
-                <StyledInput inputStyle = 'default' inputSize = 'md-max' placeholder="E-mail"></StyledInput>
-                <StyledInput inputStyle = 'default' inputSize = 'md-max' placeholder="Senha"></StyledInput>
-                <StyledButton buttonType='primary' buttonSize='md-max'>Entrar</StyledButton>
-                <StyledParagraph fontStyle='sm'>
-                Não é cadastrado?
-                </StyledParagraph>
-                <StyledLinkLoginPage to="/register">Cadastre-se</StyledLinkLoginPage>
-            </StyledDivInputLogin>
-        </StyledDivLoginPage>
-        <Footer/>
-      </StyledContainerMain>
-    )
-}
+          </StyledParagraph>
+          <StyledInput
+            inputStyle='default'
+            inputSize='md-max'
+            placeholder='E-mail'
+            className='width80'
+          ></StyledInput>
+          <StyledInput
+            inputStyle='default'
+            inputSize='md-max'
+            placeholder='Senha'
+            className='width80'
+          ></StyledInput>
+          <StyledButton buttonType='primary' buttonSize='md-max' className='width80'>
+            Entrar
+          </StyledButton>
+          <StyledParagraph fontStyle='sm'>Não é cadastrado?</StyledParagraph>
+          <StyledLinkLoginPage to='/register'>Cadastre-se</StyledLinkLoginPage>
+        </StyledDivInputLogin>
+      </StyledDivLoginPage>
+      <Footer />
+    </StyledContainerMain>
+  );
+};
